fb599bfac5af4015d3dc18f2a9cfb9cc9038af64
NAZMUS SAKIB <sakibabir52@gmail.com>
2022-03-26 14:03:15 +0600

    logo added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import logo from '../../images/logo.png';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className="header">
+            <img src={logo} alt="" />
         </div>
     );
 };
