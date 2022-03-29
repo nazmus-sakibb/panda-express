8e964567b6e9013391432b8c4540a23e2bd7ab31
NAZMUS SAKIB <sakibabir52@gmail.com>
2022-03-30 01:01:03 +0600

    cart component created

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,7 +2,13 @@ import React from 'react';
 import './Cart.css';
 
 const Cart = ({ cart }) => {
-    
+    console.log(cart);
+
+    let total = 0;
+    for(const product of cart){
+        total = total + product.price;
+    }
+
     return (
         <div>
             <h3>Order Summary</h3>
