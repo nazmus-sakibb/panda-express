9f258a873f36584dd88b76bd556e2a0cda07ee4f
NAZMUS SAKIB <sakibabir52@gmail.com>
2022-03-31 01:55:58 +0600

    cart icon added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 import './Product.css';
 
 const Product = ({ product, handleAddToCart }) => {
@@ -14,7 +16,7 @@ const Product = ({ product, handleAddToCart }) => {
                 <p><small>by: {seller}</small></p>
                 <p>Price: {price}</p>
                 <p><small>Only {stock} left in stock - order soon</small></p>
-                <button className="btn-regular" onClick={() => handleAddToCart(product)}>Add To Cart</button>
+                <button className="btn-regular" onClick={() => handleAddToCart(product)}><FontAwesomeIcon icon={faCartShopping}/> Add To Cart</button>
             </div>
         </div >
     );
